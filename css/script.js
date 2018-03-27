bc3641c3aa3c5cdaccef13a5894fbde3a127e279
britt g <bgedling6@gmail.com>
2018-03-26 23:16:34 -0400

    whole new round

---
+++ css/script.js
@@ -0,0 +1,26 @@
+// Show/Hide for Dynamic Menus //
+
+$(document).ready(function) { 
+    $("button").click(function()    {
+        $("button").removeClass("selected");
+        $(this).addClass("selected");
+        var menu = $(this).text();
+        var options = {
+            
+        }
+    )};
+    
+    
+    
+    
+    
+//   $("#Appetizers").click(function() {
+//          $("p2").hide();
+//    });
+//    $("#Appetizers").click(function() {
+//        $("p2").show();
+//    });
+//}); //
+
+// /Show/Hide for Dynamic Menus //
+

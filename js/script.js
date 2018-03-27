a06a9c88ba97fe0fb5ea5cca83ffa20a812d41c8
britt g <bgedling6@gmail.com>
2018-03-27 18:08:14 -0400

    file rework

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// lazy show hide js to avoid display classes //
+
+$(document).ready(function()  {
+
+    $(".magic-button").click(function() {
+        $(".contact").show();
+        $(".magic-button").hide();
+    });
+});

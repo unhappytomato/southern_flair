119d997efdcae25b1463b520fa4065c4969366b2
britt g <34946845+unhappytomato@users.noreply.github.com>
2018-03-27 18:09:50 -0400

    Delete script.js
    file rework- js file put in folder for proper class structure

--- script.js
+++
@@ -1,9 +0,0 @@
-// lazy show hide to avoid display classes //
-
-$(document).ready(function()  {
-
-    $(".magic-button").click(function() {
-        $(".contact").show();
-        $(".magic-button").hide();
-    });
-});

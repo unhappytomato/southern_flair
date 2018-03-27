6f186da3dc61a45ae4af0933ee6d099be4bfc4c4
britt g <bgedling6@gmail.com>
2018-03-27 00:30:18 -0400

    fuuuuuuuuuuuuuc. that was tedious.

---
+++ script.js
@@ -0,0 +1,9 @@
+// lazy show hide to avoid display classes //
+
+$(document).ready(function()  {
+
+    $(".magic-button").click(function() {
+        $(".contact").show();
+        $(".magic-button").hide();
+    });
+});
